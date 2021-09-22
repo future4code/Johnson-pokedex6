ff2b57951b68ae46b72967901289df1a53783106
Ivanilo Junior <ivanilojunior@live.com>
2021-09-21 21:40:19 -0300

    a

--- pokedex/src/HomePage.js
+++ pokedex/src/HomePage.js
@@ -17,7 +17,7 @@ export const HomePage = () =>{
         <DivHome>
             <Header>
             </Header>
-            <PokemonCard></PokemonCard>
+            <PokemonCard>bla</PokemonCard>
         </DivHome>
     )
 }
